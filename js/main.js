33a64150b765cb0e27c9673992ef6d10f82d00b7
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-01-13 15:18:33 +0300

    vaildtion inital

--- js/main.js
+++ js/main.js
@@ -8,6 +8,25 @@ function ChangeColor(){
     var bodyGround = document.getElementById("body");
     bodyGround.style.backgroundColor = SeletColor;
 }
+const form = document.getElementById('form');
+const username = document.getElementById('form');
+const email = document.getElementById('form');
+const password = document.getElementById('form');
+const password2 = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+checkInputs();
+});
+
+function checkInputs() {
+const usernameValue = username.value
+const emailValue = email.value
+const passwordValue = password.value
+const password2Value = password.
+}
+
+
 function validFrom(){
     var ageValid = document.getElementById("age").value;
     if(ageValid == less){
