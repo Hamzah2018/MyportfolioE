17d92cc69d2bfd221c95b05d786f2ae94b803980
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-01-16 02:33:19 +0300

    Some changes:In  the interfaces

--- js/main.js
+++ js/main.js
@@ -3,41 +3,71 @@
 //     var bodyGround = document.getElementById("body");
 //    bodyGround.style.backgroundColor = SeletColor;
 // }
+let ValidationForm = document.getElementById('validation');
+let btnAllowed = document.getElementById('btn-allowed');
+let checkIcon = document.getElementById('check-icon');
+let exclamIcon = document.getElementById('exclam-icon');
+let errorMassge = document.getElementById('errormassage');
+
+
+
+
+btnAllowed.addEventListener('click',(e) =>{
+    e.preventDefault();
+        //    preventDefault;
+        ValidationForm.style.visibility ="hidden";
+        checkIcon.style.visibility ="hidden";
+        exclamIcon.style.visibility ="hidden";
+        errorMassge.style.visibility ="hidden";
+}    
+);
 function ChangeColor(){
     var SeletColor = document.getElementById("selectColor").value;
     var bodyGround = document.getElementById("body");
     bodyGround.style.backgroundColor = SeletColor;
 }
 const form = document.getElementById('form');
-const username = document.getElementById('form');
-const email = document.getElementById('form');
-const password = document.getElementById('form');
-const password2 = document.getElementById('form');
+const user = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-form.addEventListener('submit', (e) => {
-   e.preventDefault();
-checkInputs();
-});
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
 
+    checkInputs();
+})
 function checkInputs() {
-const usernameValue = username.value.trem();
-const emailValue = email.value.trem();
-const passwordValue = password.value.trem();
-const password2Value = password2.vaule.trem();;
-if(usernameVlaue === ''){
-    setErrorFor(username, 'Username cannot be blank')
+    const userValue = user.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    if(userValue === ''){
+      setErrorFor(user,'Username can \'t be blank')
     }
     else{
-    setSuccessFor(username)
+        // setSuccessFor(username)
     }
-    
-} 
+}
 function setErrorFor(input, message){
-    const formControl = input.parentElement;// .form-control
-    const small = fromControl.querySelector('small');
-    }
-    
+const formControl = input.parentElement;
+const small = formControl.querySelector('small');
+small.innerText = message;
+formControl.className = 'form-control error';
+}
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.checkName = 'form-control success';
+}
+// function isEmail(email) {
+//  retrun /   
+// }
+// function validateEmail() {
+//  var mail = document.links[0].onmouseenter = fun
+// }
 
+
+    
 
 // function validFrom(){
 //     var ageValid = document.getElementById("age").value;
